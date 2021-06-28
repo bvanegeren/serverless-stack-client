563e9fedbdda2a13bea2f4306687805d0f08ffc8
Bradley Van Egeren <bvanegeren@Bradleys-MacBook-Pro.local>
2021-06-27 22:06:30 -0600

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 const config = {
+  STRIPE_KEY: "pk_test_51J0XUrFHaS50Ywh8Fu52sIjB36YkxWLkxaaARg9USnp5V2xdHIaWzZd750lkiCtil40xh5CBmm6NGFDrQVb12nFW00hDthyO6N",
+MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-2",
     BUCKET: "notes-doc-uploads",
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://aw21tc8e5b.execute-api.us-east-2.amazonaws.com/prod/notes",
+    URL: "https://aw21tc8e5b.execute-api.us-east-2.amazonaws.com/prod",//THIS USED TO HAVE /NOTES AT THE END
   },
   cognito: {
     REGION: "us-east-2",
